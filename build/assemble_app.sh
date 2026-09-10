b558ed831b68785011208326dfacfec8ef634bf8
您的名称 <>
2026-09-10 13:58:47 +0800

    0.4.18 版本号升级

--- build/assemble_app.sh
+++ build/assemble_app.sh
@@ -6,7 +6,7 @@ set -e
 BASE="/Users/vetar/Desktop/beta/subagent"
 OUT="$BASE/build/VetarAI.app"
 ELECTRON="$BASE/node_modules/electron/Electron.app"
-VERSION="0.4.17"
+VERSION="0.4.18"
 
 echo "[1/6] 清理旧产物..."
 rm -rf "$OUT"
